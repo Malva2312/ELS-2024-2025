d47f5cb71dc03f141a9a4a353c5f93f155debf4e
Sofia Teixeira <up201806629@fe.up.pt>
2024-10-02 16:35:46 +0100

    Add readFile

--- src/pt/up/fe/els2024/Main.java
+++ src/pt/up/fe/els2024/Main.java
@@ -1,8 +1,13 @@
 package pt.up.fe.els2024;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		// Hardcode the filename here
+		String filename = "assignment1Files/decision_tree_1.json";
+
+		// Call the ReadFile class method to read the file
+		File.getFileInfo(filename);
+		File.readFile(filename);
 	}
 }
