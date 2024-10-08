fdedba4cc209660b3e5af57643d2a973a153089a
Malva2312 <up202006605@edu.fe.up.pt>
2024-10-08 09:34:02 +0100

    Added const restraint

--- src/pt/up/fe/els2024/Node.java
+++ src/pt/up/fe/els2024/Node.java
@@ -2,14 +2,14 @@ package pt.up.fe.els2024;
 
 public class Node {
 
-    private int left; //index of left child, -1 if leaf
-    private int right; //index of right child, -1 if leaf
-    private int feature; //index of the feature used for split, -2 if leaf
+    private final int left; //index of left child, -1 if leaf
+    private final int right; //index of right child, -1 if leaf
+    private final int feature; //index of the feature used for split, -2 if leaf
 
-    private double threshold; //threshold for split
-    private double impurity; //impurity of the node
-    private long n_node_samples; //number of samples in the node
-    private double weighted_n_node_samples; //weighted number of samples in the node
+    private final double threshold; //threshold for split
+    private final double impurity; //impurity of the node
+    private final long n_node_samples; //number of samples in the node
+    private final double weighted_n_node_samples; //weighted number of samples in the node
 
     public Node(int left, int right, int feature, double threshold, double impurity, long n_node_samples, double weighted_n_node_samples) {
         this.left = left;
