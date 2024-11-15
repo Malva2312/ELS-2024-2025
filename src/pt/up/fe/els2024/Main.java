717bed71cd8e8ac4bd0ec991c7be90d4795bdf51
Malva2312 <up202006605@edu.fe.up.pt>
2024-11-15 15:34:37 +0000

    demostration

--- src/pt/up/fe/els2024/Main.java
+++ src/pt/up/fe/els2024/Main.java
@@ -5,45 +5,45 @@ import pt.up.fe.els2024.Builder.DataBaseBuilder;
 public class Main {
 	public static void main(String[] args) {
 		DataBaseBuilder db = new DataBaseBuilder();
-		try {
-			db
-					.processFolders()
-					.folders("assignment2Files/run1", "assignment2Files/run2", "assignment2Files/run3") // Specify folders
-					.operations(() -> { // Define reusable operation sequence
-						db.loadJSON()
-								.from("profiling.json")
-								.into("time")
-								.withAttributes("time%", "seconds", "name")
+		db
+			.processFolders()
+			.folders("assignment2Files/run1", "assignment2Files/run2", "assignment2Files/run3") // Specify folders
+			.operations(() -> { // Define reusable operation sequence
+				db.loadJSON()
+						.from("profiling.json")
+						.into("time")
+						.withAttributes("time%", "seconds", "name")
 
-								.loadXML()
-								.from("vitis-report.xml")
-								.into("vitis")
-								.nestedIn("AreaEstimates", "Resources")
+						.loadXML()
+						.from("vitis-report.xml")
+						.into("vitis")
+						.nestedIn("AreaEstimates", "Resources")
 
-								.loadYAML()
-								.from("decision_tree.yaml")
-								.into("decision_tree")
+						.loadYAML()
+						.from("decision_tree.yaml")
+						.into("decision_tree")
 
-								.loadYAML()
-								.from("decision_tree.yaml")
-								.into("decision_tree2")
-								.nestedIn("params")
+						.loadYAML()
+						.from("decision_tree.yaml")
+						.into("decision_tree2")
+						.nestedIn("params")
 
-								.selectMax()
-								.onColumn("time%")
-								.onTable("time")
+						.selectMax()
+						.onColumn("time%")
+						.onTable("time")
 
-								.concactHorizontal()
-								.toTable("final")
-								.onTables("decision_tree", "decision_tree2", "time", "vitis")
+						.concactHorizontal()
+						.toTable("final")
+						.onTables("decision_tree", "decision_tree2", "time", "vitis")
 
-								.end();
-					})
-					.printTable("final")
-					.end();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+						.end();
+				})
+				.save()
+				.tables("final")
+				.to("assignment2Files/output.html")
+				.as("html")
 
-	}
+				.end();
+
+}
 }
