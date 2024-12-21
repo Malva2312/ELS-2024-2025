0e90cf1cc8379ef2aa3fd06551404bc0fed25646
Sofia Teixeira <up201806629@edu.fe.up.pt>
2024-12-21 23:42:47 +0000

    Get all elements and features

--- src/pt/up/fe/els2024/Main.java
+++ src/pt/up/fe/els2024/Main.java
@@ -2,6 +2,7 @@ package pt.up.fe.els2024;
 
 import com.google.inject.Inject;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -11,6 +12,7 @@ import org.xtext.example.mydsl.myDsl.TopLevelOperation;
 import pt.up.fe.els2024.Builder.DataBaseBuilder;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 
@@ -50,62 +52,226 @@ public class Main {
 
 		while (treeIterator.hasNext()) {
 			var element = treeIterator.next();
-			System.out.println(element);
 
-			if (element instanceof TopLevelOperation operation) {
-				switch (operation.getClass().getSimpleName()) {
-					case "ArgMaxOperation":
-						dbBuilder.selectMax().end();
-						break;
-					case "ArgMinOperation":
-						dbBuilder.selectMin().end();
-						break;
-					case "ConcatHorizontalOperation":
-						dbBuilder.concactHorizontal().end();
-						break;
-					case "DropOperation":
-						dbBuilder.drop().end();
-						break;
-					case "FilterOperation":
-						dbBuilder.filter().end();
-						break;
-					case "JoinOperation":
-						dbBuilder.joinTables("table1", "table2").end();
-						break;
-					case "LimitOperation":
-						dbBuilder.limit().end();
-						break;
-					case "LoadJSONOperation":
-						dbBuilder.loadJSON().end();
-						break;
-					case "LoadXMLOperation":
-						dbBuilder.loadXML().end();
-						break;
-					case "LoadYAMLOperation":
-						dbBuilder.loadYAML().end();
-						break;
-					case "PrintAllOperation":
-						dbBuilder.printAll().end();
-						break;
-					case "PrintTableOperation":
-						dbBuilder.printTable("tableName").end();
-						break;
-					case "ProcessFoldersOperation":
-						dbBuilder.processFolders().end();
-						break;
-					case "RenameOperation":
-						dbBuilder.rename().end();
-						break;
-					case "SaveOperation":
-						dbBuilder.save().end();
-						break;
-					case "SelectColumnsOperation":
-						dbBuilder.select().end();
-						break;
-					default:
-						System.out.println("Unknown Operation");
-				}
+			EObject eObject = (EObject) element; // Cast your element to EObject
+
+			switch (element.getClass().getSimpleName()) {
+				case "ModelImpl":
+					//System.out.println("Model");
+					break;
+				case "TopLevelOperationImpl":
+					//System.out.println("TopLevelOperation");
+					break;
+				case "LoadJSONImpl":
+					System.out.println(".loadJSON()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("file")) {
+							System.out.println(".from(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("table")) {
+							System.out.println(".into(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("attributes")) {
+							Object nested = eObject.eGet(feature).toString();
+							// Convert "[Device, Resources]" to "Device", "Resources"
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".withAttributes(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						} else if (feature.getName().equals("nested")) {
+							Object nested = eObject.eGet(feature).toString();
+							// Convert "[Device, Resources]" to "Device", "Resources"
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".nestedIn(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						} else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "LoadXMLImpl":
+					System.out.println(".loadXML()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("file")) {
+							System.out.println(".from(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("table")) {
+							System.out.println(".into(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("nested")) {
+							Object nested = eObject.eGet(feature).toString();
+							// Convert "[Device, Resources]" to "Device", "Resources"
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".nestedIn(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						} else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "LoadYAMLImpl":
+					System.out.println(".loadYAML()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("file")) {
+							System.out.println(".from(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("table")) {
+							System.out.println(".into(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("nested")){
+							Object nested = eObject.eGet(feature).toString();
+							// Convert "[Device, Resources]" to "Device", "Resources"
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".nestedIn(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						} else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "ConcatOperationImpl":
+					System.out.println(".concatHorizontal()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("target")) {
+							System.out.println(".toTable(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("tables")) {
+							Object nested = eObject.eGet(feature).toString();
+							// Convert "[Device, Resources]" to "Device", "Resources"
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".onTables(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						} else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "FilterOperationImpl":
+					System.out.println("Filter");
+					break;
+				case "SaveOperationImpl":
+					System.out.println(".save()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("file")) {
+							System.out.println(".to(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("format")) {
+							System.out.println(".as(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("tables")){
+							Object nested = eObject.eGet(feature).toString();
+							String nestedStr = (String) nested;
+							String processed = nestedStr
+									.replaceAll("[\\[\\]]", "") // Remove square brackets
+									.replaceAll("\\s+", "");   // Remove extra spaces
+							String[] parts = processed.split(","); // Split into individual elements
+
+							// Generate output using string concatenation
+							String result = ".tables(";
+							for (int i = 0; i < parts.length; i++) {
+								result += "\"" + parts[i] + "\"";
+								if (i < parts.length - 1) {
+									result += ", "; // Add a comma and space between elements
+								}
+							}
+							result += ")";
+							System.out.println(result);
+						}else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "PrintOperationImpl":
+					System.out.println("Print");
+					break;
+				case "PrintAllOperationImpl":
+					System.out.println("PrintAll");
+					break;
+				case "PrintTableOperationImpl":
+					System.out.println("PrintTable");
+					break;
+				case "RenameOperationImpl":
+					System.out.println("Rename");
+					break;
+				case "LimitOperationImpl":
+					System.out.println("Limit");
+					break;
+				case "ArgMaxOperationImpl":
+					System.out.println(".selectMax()");
+					for (var feature : eObject.eClass().getEAllStructuralFeatures()) {
+						if (feature.getName().equals("column")){
+							System.out.println(".onColumn(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("table")) {
+							System.out.println(".onTable(\"" + eObject.eGet(feature) + "\")");
+						} else if (feature.getName().equals("target")) {
+							System.out.println(".toTable(\"" + eObject.eGet(feature) + "\")");
+						} else System.out.println("." + feature.getName() + "(\"" + eObject.eGet(feature) + "\")");
+					}
+					break;
+				case "ArgMinOperationImpl":
+					System.out.println("ArgMin");
+					break;
+				case "SelectOperationImpl":
+					System.out.println("Select");
+					break;
+				case "DropOperationImpl":
+					System.out.println("Drop");
+					break;
+				case "ProcessFoldersOperationImpl":
+					System.out.println("ProcessFolders");
+					break;
+				case "EndOperationImpl":
+					//System.out.println(".end()");
+					break;
+				default:
+					System.out.println("Unknown Operation");
 			}
+			System.out.println("");
 		}
 	}
 
